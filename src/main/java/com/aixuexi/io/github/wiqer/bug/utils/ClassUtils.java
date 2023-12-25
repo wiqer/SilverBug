53fefa7854961f8eb65c16a913cca0832e655b4f
lilanfeng <lilanfeng@aixuexi.com>
2023-12-25 13:36:34 +0800

    ClassUtils

---
+++ src/main/java/com/aixuexi/io/github/wiqer/bug/utils/ClassUtils.java
@@ -0,0 +1,182 @@
+package com.aixuexi.io.github.wiqer.bug.utils;
+
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.lang.annotation.Annotation;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+/**
+ * ：ClassUtils
+ *
+ * @author ：李岚峰、lilanfeng、
+ * @device name ：user
+ * @date ：Created in 25 / 2023/12/25  13:34
+ * @description：
+ * @modified By：
+ */
+public final class ClassUtils {
+    /**
+     * 定义类集合（存放基础包名下的所有类）
+     */
+    private static final Set<Class<?>> CLASS_SET;
+    private static final String basePackage = "com.wiqer.mole";
+    static {
+        //获取基础包名
+
+        //获取基础包名下所有类
+        CLASS_SET = getClassSet(basePackage);
+    }
+    /**
+     * 获取类加载器
+     */
+    public static ClassLoader getClassLoader() {
+        return Thread.currentThread().getContextClassLoader();
+    }
+
+    /**
+     * 加载类
+     * @param className 类名
+     * @param isInitialized 是否初始化
+     * @return
+     */
+    public static Class<?> loadClass(String className, boolean isInitialized) {
+        Class<?> cls;
+        try {
+            cls = Class.forName(className, isInitialized, getClassLoader());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return cls;
+    }
+
+    /**
+     * 加载类（默认将初始化类）
+     */
+    public static Class<?> loadClass(String className) {
+        return loadClass(className, true);
+    }
+
+    /**
+     * 获取指定包名下的所有类
+     */
+    public static Set<Class<?>> getClassSet(String packageName) {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        try {
+            Enumeration<URL> urls = getClassLoader().getResources(packageName.replace(".", "/"));
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                if (url != null) {
+                    String protocol = new String(url.getProtocol().getBytes(StandardCharsets.UTF_8)) ;
+                    if (protocol.equals("file")) {
+//                        String package1= new String( url.getPath().getBytes(StandardCharsets.UTF_8)) ;
+                        String packagePath =   URLDecoder.decode(url.getPath(), String.valueOf(StandardCharsets.UTF_8));//new String( url.getPath().replaceAll("%20", " ").getBytes("gbk") , StandardCharsets.UTF_8);
+                        addClass(classSet, packagePath, packageName);
+                    } else if (protocol.equals("jar")) {
+                        JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
+                        if (jarURLConnection != null) {
+                            JarFile jarFile = jarURLConnection.getJarFile();
+                            if (jarFile != null) {
+                                Enumeration<JarEntry> jarEntries = jarFile.entries();
+                                while (jarEntries.hasMoreElements()) {
+                                    JarEntry jarEntry = jarEntries.nextElement();
+                                    String jarEntryName = jarEntry.getName();
+                                    if (jarEntryName.endsWith(".class")) {
+                                        String className = jarEntryName.substring(0, jarEntryName.lastIndexOf(".")).replaceAll("/", ".");
+                                        doAddClass(classSet, className);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return classSet;
+    }
+    public static void mergeClassSet(String packageName){
+        if(StringUtils.isNotEmpty( packageName)&&!packageName.equals(basePackage)){
+            CLASS_SET.addAll(getClassSet(packageName));
+        }
+
+    }
+    private static void addClass(Set<Class<?>> classSet, String packagePath, String packageName) {
+        File[] files = new File(packagePath).listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return (file.isFile() && file.getName().endsWith(".class")) || file.isDirectory();
+            }
+        });
+        for (File file : files) {
+            String fileName = file.getName();
+            if (file.isFile()) {
+                String className = fileName.substring(0, fileName.lastIndexOf("."));
+                if (org.apache.commons.lang3.StringUtils.isNotEmpty(packageName)) {
+                    className = packageName + "." + className;
+                }
+                doAddClass(classSet, className);
+            } else {
+                String subPackagePath = fileName;
+                if (org.apache.commons.lang3.StringUtils.isNotEmpty(packagePath)) {
+                    subPackagePath = packagePath + "/" + subPackagePath;
+                }
+                String subPackageName = fileName;
+                if (StringUtils.isNotEmpty(packageName)) {
+                    subPackageName = packageName + "." + subPackageName;
+                }
+                addClass(classSet, subPackagePath, subPackageName);
+            }
+        }
+    }
+
+    private static void doAddClass(Set<Class<?>> classSet, String className) {
+        Class<?> cls = loadClass(className, false);
+        classSet.add(cls);
+    }
+    /**
+     * 获取基础包名下的所有类
+     */
+    public static Set<Class<?>> getClassSet() {
+        return CLASS_SET;
+    }
+
+
+    /**
+     * 获取基础包名下某父类的所有子类 或某接口的所有实现类
+     */
+    public static Set<Class<?>> getClassSetBySuper(Class<?> superClass) {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for (Class<?> cls : CLASS_SET) {
+            //isAssignableFrom() 指 superClass 和 cls 是否相同或 superClass 是否是 cls 的父类/接口
+            if (superClass.isAssignableFrom(cls) && !superClass.equals(cls)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    /**
+     * 获取基础包名下带有某注解的所有类
+     */
+    public static Set<Class<?>> getClassSetByAnnotation(Class<? extends Annotation> annotationClass) {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for (Class<?> cls : CLASS_SET) {
+            if (cls.isAnnotationPresent(annotationClass)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+}
+
