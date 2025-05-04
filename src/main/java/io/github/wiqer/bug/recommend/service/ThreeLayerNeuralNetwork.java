382aa3ddd7c27250a87bd71dfcfe2551f0f5f243
llf <lilanfeng1995@hotmail.com>
2025-05-05 02:14:47 +0800

    复习整理神经网络部分

--- src/main/java/io/github/wiqer/bug/recommend/service/ThreeLayerNeuralNetwork.java
+++ src/main/java/io/github/wiqer/bug/recommend/service/ThreeLayerNeuralNetwork.java
@@ -51,32 +51,38 @@ public class ThreeLayerNeuralNetwork {
     public static void main(String[] args) {
         // 示例用法
         // 创建一个具有2个输入节点、3个隐藏节点1、2个隐藏节点2、1个输出节点的三层神经网络
-        ThreeLayerNeuralNetwork neuralNetwork = new ThreeLayerNeuralNetwork(4, 4,2, 1 ,4,4, 4,0.1);
+        ThreeLayerNeuralNetwork neuralNetwork = new ThreeLayerNeuralNetwork(4, 4,2, 2 ,4,4, 4,0.1);
 
         // 训练数据
         double[][] trainingData = {
-                {0, 6, 6, 0, 0.8},
-                {0, 6, 6, 1, 0.4},
-                {0, 6, 6, 1, 0.4},
-                {5, 6, 6, 1, 0.7},
-                {3, 6, 6, 1, 0.5},
-                {1, 6, 6, 0, 0.1},
-                {0, 6, 6, 1, 0.4},
-                {1, 6, 6, 1, 0.2}
+                {0, 6, 6, 0, 0.8, 0.1},
+                {0, 6, 6, 1, 0.4, 0.4},
+                {0, 6, 6, 1, 0.4, 0.4},
+                {5, 6, 6, 1, 0.7, 0.8},
+                {3, 6, 6, 1, 0.5, 0.9},
+                {1, 6, 6, 0, 0.1, 0.8},
+                {0, 6, 6, 1, 0.4, 0.4},
+                {1, 6, 6, 1, 0.2, 0.9}
         };
 
         // 进行训练
         for (int epoch = 0; epoch < 10000; epoch++) {
             for (double[] data : trainingData) {
                 double[] inputs = {data[0], data[1], data[2], data[3]};
-                double[] targets = {data[4]};
+                double[] targets = {data[4],data[5]};
                 neuralNetwork.train(inputs, targets);
             }
         }
 
+        long time = System.currentTimeMillis();
         // 进行预测
-        double[] inputs = {0, 6,6, 1};
+        double[] inputs = {0, 6, 6, 1};
         double[] outputs = neuralNetwork.forwardPropagation(inputs);
-        System.out.println("Predicted output: " + outputs[0]);
+        double[] inputs2 = {5, 6, 6, 1};
+        double[] outputs2 = neuralNetwork.forwardPropagation(inputs);
+        double[] inputs3 = {3, 6, 6, 1};
+        double[] outputs3 = neuralNetwork.forwardPropagation(inputs);
+        System.out.println("Predicted output: " + outputs[0] +"，Predicted output2: " + outputs2[0]+"，Predicted output3: " + outputs3[0] + ". used time: " + (System.currentTimeMillis() - time));
+        System.out.println("Predicted output: " + outputs[1] +"，Predicted output2: " + outputs2[1]+"，Predicted output3: " + outputs3[1] + ". used time: " + (System.currentTimeMillis() - time));
     }
 }
