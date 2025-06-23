5288031ff40742ea5801d89d5988c8a945c65326
lilanfeng <lanfeng.li@tapstitch.com>
2025-06-23 18:34:06 +0800

    优化BugEnableCustomServices启动类：精确扫描manage包、使用配置类封装、完善注释，提升性能与可维护性

--- src/main/java/io/github/wiqer/bug/BugEnableCustomServices.java
+++ src/main/java/io/github/wiqer/bug/BugEnableCustomServices.java
@@ -1,29 +1,34 @@
 package io.github.wiqer.bug;
 
-/**
- * ：BaseEnableCustomServices
- *
- * @author ：李岚峰、lilanfeng、
- * @device name ：user
- * @date ：Created in 22 / 2024/1/22  14:15
- * @description：
- * @modified By：
- */
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
+import org.springframework.context.annotation.Import;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * BugEnableCustomServices - 启用Bug能力自定义服务
+ * 自动扫描和注册io.github.wiqer.bug.manage包下的所有Service和Component
+ *
+ * @author 李岚峰
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@ComponentScan(
-        basePackages = "io.github.wiqer.bug",
-        includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Service.class,Component.class})
-)
+@Import(BugEnableCustomServices.BugCustomServicesConfiguration.class)
 public @interface BugEnableCustomServices {
+    
+    /**
+     * Bug自定义服务配置类
+     */
+    @org.springframework.context.annotation.Configuration
+    @org.springframework.context.annotation.ComponentScan(
+        basePackages = "io.github.wiqer.bug.manage",
+        includeFilters = @org.springframework.context.annotation.ComponentScan.Filter(
+            type = org.springframework.context.annotation.FilterType.ANNOTATION, 
+            classes = {org.springframework.stereotype.Service.class, org.springframework.stereotype.Component.class}
+        )
+    )
+    class BugCustomServicesConfiguration {
+        // 配置类，用于启用组件扫描
+    }
 }
